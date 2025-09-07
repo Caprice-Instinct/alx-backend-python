9c3e51500d4fa954492010c008900c9546f25aa6
Caprice-Instinct <linetwangui04@gmail.com>
2025-09-07 22:29:13 +0300

    batchprocessing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,28 @@
+
+import sqlite3
+
+def stream_users_in_batches(batch_size):
+	connection = sqlite3.connect('ALX_prodev.db')
+	connection.row_factory = sqlite3.Row
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM user_data")
+	batch = []
+	for row in cursor:
+		batch.append(dict(row))
+		if len(batch) == batch_size:
+			yield batch
+			batch = []
+	if batch:
+		yield batch
+	cursor.close()
+	connection.close()
+
+def batch_processing(batch_size):
+	for batch in stream_users_in_batches(batch_size):
+		for user in batch:
+			if user['age'] > 25:
+				print(user)
+
+
+if __name__ == "__main__":
+	batch_processing(3)
