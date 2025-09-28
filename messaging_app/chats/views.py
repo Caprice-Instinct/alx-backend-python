f96d721ee7b18385201fb07e3d43b6f46887c41a
Caprice-Instinct <linetwangui04@gmail.com>
2025-09-28 10:46:16 +0300

    feat: add serializers for User, Conversation, and Message with nested relationships

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
-# Create your views here.
+# User ViewSet
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Message ViewSet
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+# Conversation ViewSet
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
