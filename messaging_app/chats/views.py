bdc692266618bf3d8f3a9d07c76949aa52b3cf77
Caprice-Instinct <linetwangui04@gmail.com>
2025-09-28 10:48:38 +0300

    feat: add DRF viewsets for conversations and messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,18 +1,33 @@
-from rest_framework import viewsets
-from .models import User, Conversation, Message
-from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import filters
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# User ViewSet
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Message ViewSet
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-
-# Conversation ViewSet
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
+
+    @action(detail=False, methods=['post'])
+    def create_conversation(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
+
+    @action(detail=False, methods=['post'])
+    def send_message(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
