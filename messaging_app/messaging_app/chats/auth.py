0fd761c9826715643f9ecfb2ab5004abcb3e1499
Caprice-Instinct <linetwangui04@gmail.com>
2025-09-28 21:43:21 +0300

    implemented authentication

---
+++ messaging_app/messaging_app/chats/auth.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import SessionAuthentication
+
+class CustomJWTAuthentication(JWTAuthentication):
+    pass  # Extend if you need custom logic
+
+class CustomSessionAuthentication(SessionAuthentication):
+    pass  # Extend if you need custom logic
